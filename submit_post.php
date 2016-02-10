17de541e7d98fa586bc58fd8c10ad94768643467
icefyre <mharris29@outlook.com>
2016-02-10 22:13:43 +0000

    Initial data entry works and saves in database. Using PDO for escaping.

--- submit_post.php
+++ submit_post.php
@@ -9,36 +9,56 @@ try
 $db = new PDO('sqlite:guestbook_PDO.sqlite');
 
 
-
 //get post data
 $name = $_POST['name'];
 $email = $_POST['email'];
 $entry = $_POST['entry'];
+$title = $_POST['title'];
+$ip = $_SERVER['REMOTE_ADDR'];
 
- 
+
+
+
 
 //create the database
 
-$db->exec("CREATE TABLE guestbook (Id INTEGER PRIMARY KEY, Date DATETIME, Name TEXT, Email TEXT, Entry TEXT)");   
+$db->exec("CREATE TABLE guestbook (Id INTEGER PRIMARY KEY, Date DATETIME, Name TEXT, Email TEXT, Title TEXT, Entry TEXT, ip_address TEXT)");   
+
+
+
+//prepare SQL query for inserting data into database
+$stmt = $db->prepare("INSERT INTO guestbook (Date, Name,Email,Title,Entry,ip_address) VALUES (datetime(),:name,:email,:title,:entry,:ip);");
+//$stmt = $dbh->prepare("INSERT INTO REGISTRY (name, value) VALUES (:name, :value)");
+$stmt->bindParam(':name', $name);
+$stmt->bindParam(':email', $email);
+$stmt->bindParam(':title', $title);
+$stmt->bindParam(':entry', $entry);
+$stmt->bindParam(':ip', $ip);
+
+$stmt->execute();
+
 
 
 
 //insert some data...
 
-
+/*
 if (name_exists($name))
 {
     die("ERROR NAME EXISTS");
 }
+*/
 
-$db->exec("INSERT INTO guestbook (Date, Name, Email,Entry) VALUES (date(),'$name','$email','$entry' );");
+//print_r($_POST);
+//$db->exec("INSERT INTO guestbook (Date, Name,Email,Title,Entry,ip_address) VALUES (datetime(),'$name','$email','$title','$entry','$ip');");
 
- 
+//debug
+//echo  "INSERT INTO guestbook (Date, Name,Email,Title,Entry,ip_address) VALUES (datetime(),'$name','$email','$title','$entry','$ip');";
 //now output the data to a simple html table...
 
 print "<table border=1>";
 
-print "<tr><td>Date</td><td>Name</td><td>email</td><td>entry</td></tr>";
+print "<tr><td>Date</td><td>Name</td><td>email</td><td>title</td><td>entry</td><td>IP address</td></tr>";
 
 $result = $db->query('SELECT * FROM guestbook');
 
@@ -53,8 +73,9 @@ print "<td>".$row['Email']."</td>";
 
 print "<td>".$row['Title']."</td>";
 
-print "<td>".$row['Entry']."</td></tr>";
+print "<td>".$row['Entry']."</td>";
 
+print "<td>".$row['ip_address']."</td></tr>";
 }
 
 print "</table>";
